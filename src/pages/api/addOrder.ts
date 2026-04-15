f3fe9ab5888a2e02ab54ceec0e478a5831128c94
patatita <44934078+patatitta@users.noreply.github.com>
2026-04-15 12:34:46 +0200

    fix removed unused file. added headless to the puppeteer launch parameters with the hopes of fixing a bug where images wouldn't display on emails sent from the AWS

--- src/pages/api/addOrder.ts
+++ src/pages/api/addOrder.ts
@@ -178,9 +178,11 @@ const homeUrl = new URL('/pdf?codigo='+codigoPedido, baseUrl);
 
 
 const browser = await puppeteer.launch({
-  args: ['--no-sandbox', '--disable-setuid-sandbox']
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  headless: true
 });
 const page = await browser.newPage();
+
 await page.goto(homeUrl.href, {
   waitUntil: 'load',
 });
