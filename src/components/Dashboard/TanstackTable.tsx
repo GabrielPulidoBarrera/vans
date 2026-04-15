1ba54e824843191fb6dd830b80a3518c789d4c1e
patatita <44934078+patatitta@users.noreply.github.com>
2026-04-15 10:07:20 +0200

    feat added pagination to the tanstack table, replaced the dashboard with the tanstack table.

--- src/components/Dashboard/TanstackTable.tsx
+++ src/components/Dashboard/TanstackTable.tsx
@@ -1,4 +1,4 @@
-import { useReactTable, getFilteredRowModel, getCoreRowModel, flexRender } from '@tanstack/react-table'
+import { useReactTable, getFilteredRowModel, getCoreRowModel, flexRender, getSortedRowModel, getPaginationRowModel } from '@tanstack/react-table'
 import { useState } from 'react';
 
 //a
@@ -9,7 +9,11 @@ const columns = [{ accessorKey: 'correo', header: 'Correo' }, { accessorKey: 'co
 
 export default function SimpleTable({data}) {
     const [globalFilter, setGlobalFilter] = useState<any>([])
-    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel(), getFilteredRowModel: getFilteredRowModel(), state: {globalFilter,},  onGlobalFilterChange: setGlobalFilter })
+    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel(), getFilteredRowModel: getFilteredRowModel(), state: {globalFilter,},  onGlobalFilterChange: setGlobalFilter, getSortedRowModel: getSortedRowModel(), getPaginationRowModel: getPaginationRowModel(), 
+   initialState: {
+    pagination: { pageSize: 10 } 
+  }
+ })
 
     return (
         <div class="w-full text-center" >
@@ -21,12 +25,12 @@ export default function SimpleTable({data}) {
             placeholder="Search all columns..."
         />
 
-        <table class="w-full text-center">
-        <thead>
+        <table class="border-solid border-gray-200 w-9/10 mx-auto ">
+        <thead class="bg-gray-200">
             {table.getHeaderGroups().map((hg) => (
-            <tr id={hg.id} key={hg.id}>
+            <tr id={hg.id} key={hg.id} >
                 {hg.headers.map((header) => (
-                <th key={header.id}>
+                <th key={header.id} onClick={header.column.getToggleSortingHandler()}>
                     {flexRender(header.column.columnDef.header, header.getContext())}
                 </th>
                 ))}
@@ -35,7 +39,7 @@ export default function SimpleTable({data}) {
         </thead>
         <tbody>
             {table.getRowModel().rows.map((row) => (
-            <tr id={row.original.id} key={row.id} data-carrito={row.original.cart}>
+            <tr id={row.original.id} key={row.id} data-carrito={row.original.cart} class="border-y-solid border border-gray-200 *:py-3 hover:bg-red-300"> 
                 {row.getVisibleCells().map((cell) => (
                 <td key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
@@ -48,6 +52,44 @@ export default function SimpleTable({data}) {
             
         </table>
 
+            <div class="flex text-center justify-center gap-4">
+                <button
+  onClick={() => table.firstPage()}
+  disabled={!table.getCanPreviousPage()}
+>
+  {'<<'}
+</button>
+<button
+  onClick={() => table.previousPage()}
+  disabled={!table.getCanPreviousPage()}
+>
+  {'<'}
+</button>
+<button
+  onClick={() => table.nextPage()}
+  disabled={!table.getCanNextPage()}
+>
+  {'>'}
+</button>
+<button
+  onClick={() => table.lastPage()}
+  disabled={!table.getCanNextPage()}
+>
+  {'>>'}
+</button>
+<select
+  value={table.getState().pagination.pageSize}
+  onChange={e => {
+    table.setPageSize(Number(e.target.value))
+  }}
+>
+  {[10, 20, 30, 40, 50].map(pageSize => (
+    <option key={pageSize} value={pageSize}>
+      {pageSize}
+    </option>
+  ))}
+</select>
+            </div>
 
     </div>
 
